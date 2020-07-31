9dac1837e88f4e01e6ad86bab332b03ddb5625d0
Jhon Guerrero <jhoguer@gmail.com>
2020-07-31 15:13:35 -0500

    Terminando Tareas

--- tareas/tareas.go
+++ tareas/tareas.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+type taskList struct {
+	tasks []*task
+}
+
+func (t *taskList) agregarALista(tl *task) {
+	t.tasks = append(t.tasks, tl)
+}
+
+func (t *taskList) eliminarDeList(index int) {
+	t.tasks = append(t.tasks[:index], t.tasks[index+1:]...)
+}
+
+func (t *taskList) imprimirLista() {
+	for _, tarea := range t.tasks {
+		fmt.Println("Nombre", tarea.nombre)
+		fmt.Println("Descripcion", tarea.descripcion)
+	}
+}
+
+func (t *taskList) imprimirListaCompletados() {
+	for _, tarea := range t.tasks {
+		if tarea.completado {
+			fmt.Println("Nombre", tarea.nombre)
+			fmt.Println("Descripcion", tarea.descripcion)
+		}
+	}
+}
+
 type task struct {
 	nombre      string
 	descripcion string
@@ -21,14 +49,44 @@ func (t *task) actuaizarNombre(nombre string) {
 }
 
 func main() {
-	t := &task{
+	t1 := &task{
 		nombre:      "Completar mi curso de Go",
 		descripcion: "Completar mi curso de Go de platzi esta semana",
 	}
 
-	fmt.Printf("%+v\n", t)
-	t.marcarCompleta()
-	t.actuaizarNombre("Finalizar mi curso de Go")
-	t.actuaizarDescripcion("Completar mi curso cuanto antes")
-	fmt.Printf("%+v\n", t)
+	t2 := &task{
+		nombre:      "Completar mi curso de Python",
+		descripcion: "Completar mi curso de Python de platzi esta semana",
+	}
+
+	t3 := &task{
+		nombre:      "Completar mi curso de NodeJs",
+		descripcion: "Completar mi curso de NodeJs de platzi esta semana",
+	}
+
+	lista := &taskList{
+		tasks: []*task{
+			t1, t2,
+		},
+	}
+
+	fmt.Println(lista.tasks[0])
+	lista.agregarALista(t3)
+	// fmt.Println(len(lista.tasks))
+	// lista.eliminarDeList(1)
+	// fmt.Println(len(lista.tasks))
+	// // fmt.Println(lista.tasks[2])
+	// for i := 0; i < len(lista.tasks); i++ {
+	// 	fmt.Println("Index", i, "Nombre", lista.tasks[i].nombre)
+	// }
+
+	// for index, tarea := range lista.tasks {
+	// 	fmt.Println("Index", index, "Nombre", tarea.nombre)
+	// }
+
+	lista.imprimirLista()
+	lista.tasks[0].marcarCompleta()
+	fmt.Println("Tareas Comletadas")
+	lista.imprimirListaCompletados()
+
 }
