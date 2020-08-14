f992e02676202e0f6fa43ba9bf5e000f618def61
Jhon Guerrero <jhoguer@gmail.com>
2020-08-13 19:54:31 -0500

    Implementando concurrencia

---
+++ concurrencia/servidores.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	inicio := time.Now()
+
+	canal := make(chan string)
+
+	servidores := []string{
+		"http://platzi.com",
+		"http://google.com",
+		"http://facebook.com",
+		"http://instagram.com",
+	}
+
+	i := 0
+
+	for {
+		if i > 10 {
+			break
+		}
+
+		for _, servidor := range servidores {
+			go revisarServidor(servidor, canal)
+		}
+
+		time.Sleep(1 * time.Second)
+		fmt.Print(i+1, " - ")
+		fmt.Println(<-canal)
+
+		i++
+	}
+
+	tiempoPaso := time.Since(inicio)
+	fmt.Printf("Tiempo de ejecucion %s\n", tiempoPaso)
+}
+
+func revisarServidor(servidor string, canal chan string) {
+	_, err := http.Get(servidor)
+	if err != nil {
+		canal <- servidor + " no se encuentra disponible++"
+	} else {
+		canal <- servidor + " esta funcionando normalmente++"
+	}
+}
